bb6f4f4ddf2d044a113e9566c71a14591716a022
Baudrillard <smbalf+222@gmail.com>
2023-04-02 07:42:41 +0100

    yes

--- player_saving.py
+++ player_saving.py
@@ -1,3 +1,25 @@
+"""
+# CLOUDPICKLE SAVING METHOD
+import cloudpickle
+import os
+
+SAVE_FILE = "save_data.pkl"
+
+def save_game(player, market):
+    save_data = {"player": player, "market": market}
+
+    with open(SAVE_FILE, "wb") as save_file:
+        cloudpickle.dump(save_data, save_file)
+
+def load_game():
+    if os.path.exists(SAVE_FILE):
+        with open(SAVE_FILE, "rb") as save_file:
+            save_data = cloudpickle.load(save_file)
+            return save_data["player"], save_data["market"]
+    else:
+        return None, None
+"""
+
 # JSON SAVING METHOD
 import json
 import os
@@ -34,26 +56,3 @@ def load_game():
         return None, None
 
 
-"""
-#PICKLE SAVING METHOD
-import pickle
-import os
-
-SAVE_FILE = "save_data.pkl"
-
-
-def save_game(player, market):
-    save_data = {"player": player, "market": market}
-
-    with open(SAVE_FILE, "wb") as save_file:
-        pickle.dump(save_data, save_file)
-
-
-def load_game():
-    if os.path.exists(SAVE_FILE):
-        with open(SAVE_FILE, "rb") as save_file:
-            save_data = pickle.load(save_file)
-            return save_data["player"], save_data["market"]
-    else:
-        return None, None
-"""
